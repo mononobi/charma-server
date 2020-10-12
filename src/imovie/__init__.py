3e496234d90cbfafe08f0a9746e729693678301e
mono <mohamadnobakht@gmail.com>
2020-10-12 22:07:47 +0330

    Revise code

--- src/imovie/__init__.py
+++ src/imovie/__init__.py
@@ -6,8 +6,23 @@ imovie package.
 from pyrin.application.base import Application
 
 
+__version__ = '0.1'
+
+
 class IMovieApplication(Application):
     """
     server should create an instance of this class on startup.
     """
-    pass
+
+    def get_application_version(self):
+        """
+        gets application version.
+
+        this method must be overridden in subclasses.
+
+        :raises CoreNotImplementedError: core not implemented error.
+
+        :rtype: str
+        """
+
+        return __version__
