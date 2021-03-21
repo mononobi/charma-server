b5dcebbe2931c099d130eb2530d08deb48965b61
mono <mohamadnobakht@gmail.com>
2021-03-21 14:26:13 +0330

    Revise code

--- src/imovie/movies/related_persons/directors/api.py
+++ src/imovie/movies/related_persons/directors/api.py
@@ -6,9 +6,9 @@ movies related directors api module.
 from pyrin.api.router.decorators import api
 from pyrin.core.enumerations import HTTPMethodEnum
 
-import imovie.movies.related_persons.directors.services as related_directors_services
+import imovie.movies.related_persons.directors.services as related_director_services
 
 
 # Usage:
 # you could implement different api functions here and call corresponding service method this way:
-# return related_directors_services.method_name(*arg, **kwargs)
+# return related_director_services.method_name(*arg, **kwargs)
