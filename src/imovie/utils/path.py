2d086d992ffa8205e5dea735b89ff12bccf55749
mono <mohamadnobakht@gmail.com>
2021-03-22 23:43:04 +0430

    Implement utils package

---
+++ src/imovie/utils/path.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+utils path module.
+"""
+
+from os import path
+
+
+def get_file_extension(file, **options):
+    """
+    gets the extension of given file.
+
+    :param str file: file path to get its extension.
+
+    :keyword bool remove_dot: specifies that the extension must not
+                              include the `.` character.
+                              defaults to True if not provided.
+
+    :rtype: str
+    """
+
+    remove_dot = options.get('remove_dot', True)
+    name, extension = path.splitext(file)
+
+    if remove_dot is not False:
+        extension = extension.replace('.', '')
+
+    return extension.lower()
