200668f6880b54e46ac2e28e55ed002adbcf25f3
mono <mohamadnobakht@gmail.com>
2021-03-23 02:19:21 +0430

    Implement opencv media info provider

--- src/imovie/media_info/handlers/opencv.py
+++ src/imovie/media_info/handlers/opencv.py
@@ -3,6 +3,8 @@
 media info handlers opencv module.
 """
 
+import cv2
+
 from imovie.media_info.decorators import media_info_provider
 from imovie.media_info.handlers.base import MediaInfoProviderBase
 
@@ -25,4 +27,26 @@ class OpenCVMediaInfoProvider(MediaInfoProviderBase):
 
         :rtype: dict
         """
-        pass
+
+        video = cv2.VideoCapture(file)
+        runtime = self._get_runtime(video)
+        height = video.get(cv2.CAP_PROP_FRAME_HEIGHT)
+        width = video.get(cv2.CAP_PROP_FRAME_WIDTH)
+
+        return dict(runtime=int(runtime), height=int(height), width=int(width))
+
+    def _get_runtime(self, video):
+        """
+        gets the runtime of given video object in minutes.
+
+        :param VideoCapture video: video object.
+
+        :rtype: int
+        """
+
+        frame_count = video.get(cv2.CAP_PROP_FRAME_COUNT)
+        frames_per_second = video.get(cv2.CAP_PROP_FPS)
+        if frames_per_second == 0:
+            return 0
+
+        return (frame_count / frames_per_second) / 60
