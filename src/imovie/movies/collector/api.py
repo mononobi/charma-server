d640dc1d5d141b51f0a3d78a3ee703004c95c98f
mono <mohamadnobakht@gmail.com>
2021-03-30 01:18:40 +0430

    Add movies.collector api

--- src/imovie/movies/collector/api.py
+++ src/imovie/movies/collector/api.py
@@ -9,6 +9,46 @@ from pyrin.core.enumerations import HTTPMethodEnum
 import imovie.movies.collector.services as movies_collector_services
 
 
-# Usage:
-# you could implement different api functions here and call corresponding service method this way:
-# return movies_collector_services.method_name(*arg, **kwargs)
+@api('/movies/collect', methods=HTTPMethodEnum.POST, authenticated=False)
+def collect(directory, **options):
+    """
+    collects the movie from given directory into database.
+
+    :param str directory: directory path of movie.
+
+    :keyword bool force: specifies that movie must be forcefully collected
+                         even if size or runtime conditions are not met.
+                         defaults to False if not provided.
+
+    :raises InvalidPathError: invalid path error.
+    :raises PathIsNotAbsoluteError: path is not absolute error.
+    :raises PathNotExistedError: path not existed error.
+    :raises IsNotDirectoryError: is not directory error.
+    :raises DirectoryIsIgnoredError: directory is ignored error.
+    :raises MovieIsAlreadyCollectedError: movie is already collected error.
+    :raises InvalidMovieTitleError: invalid movie title error.
+    :raises DirectoryIsEmptyError: directory is empty error.
+    """
+
+    return movies_collector_services.collect(directory, **options)
+
+
+@api('/movies/collect_all', methods=HTTPMethodEnum.POST, authenticated=False)
+def collect_all(root, **options):
+    """
+    collects all movies in root directory.
+
+    :param str root: root directory.
+
+    :keyword bool include_individual_files: specifies that individual movie files in the
+                                            root path that have no directory must also be
+                                            collected as movies.
+                                            defaults to True if not provided.
+
+    :raises InvalidPathError: invalid path error.
+    :raises PathIsNotAbsoluteError: path is not absolute error.
+    :raises PathNotExistedError: path not existed error.
+    :raises IsNotDirectoryError: is not directory error.
+    """
+
+    return movies_collector_services.collect_all(root, **options)
--- src/imovie/movies/collector/services.py
+++ src/imovie/movies/collector/services.py
@@ -8,6 +8,68 @@ from pyrin.application.services import get_component
 from imovie.movies.collector import MoviesCollectorPackage
 
 
-# Usage:
-# you could implement different services here and call corresponding manager method this way:
-# return get_component(MoviesCollectorPackage.COMPONENT_NAME).method_name(*arg, **kwargs)
+def collect(directory, **options):
+    """
+    collects the movie from given directory into database.
+
+    :param str directory: directory path of movie.
+
+    :keyword bool force: specifies that movie must be forcefully collected
+                         even if size or runtime conditions are not met.
+                         defaults to False if not provided.
+
+    :raises InvalidPathError: invalid path error.
+    :raises PathIsNotAbsoluteError: path is not absolute error.
+    :raises PathNotExistedError: path not existed error.
+    :raises IsNotDirectoryError: is not directory error.
+    :raises DirectoryIsIgnoredError: directory is ignored error.
+    :raises MovieIsAlreadyCollectedError: movie is already collected error.
+    :raises InvalidMovieTitleError: invalid movie title error.
+    :raises DirectoryIsEmptyError: directory is empty error.
+    """
+
+    return get_component(MoviesCollectorPackage.COMPONENT_NAME).collect(directory, **options)
+
+
+def collect_all(root, **options):
+    """
+    collects all movies in root directory.
+
+    :param str root: root directory.
+
+    :keyword bool include_individual_files: specifies that individual movie files in the
+                                            root path that have no directory must also be
+                                            collected as movies.
+                                            defaults to True if not provided.
+
+    :raises InvalidPathError: invalid path error.
+    :raises PathIsNotAbsoluteError: path is not absolute error.
+    :raises PathNotExistedError: path not existed error.
+    :raises IsNotDirectoryError: is not directory error.
+    """
+
+    return get_component(MoviesCollectorPackage.COMPONENT_NAME).collect_all(root, **options)
+
+
+def get_quality(width, height, **options):
+    """
+    gets the video quality based on given resolution.
+
+    :param int width: width.
+    :param int height: height.
+
+    :returns: video quality.
+    :enum video quality:
+        UNKNOWN = 0
+        VCD = 1
+        DVD = 2
+        HD = 3
+        FHD = 4
+        QHD = 5
+        UHD = 6
+
+    :rtype: int
+    """
+
+    return get_component(MoviesCollectorPackage.COMPONENT_NAME).get_quality(width, height,
+                                                                            **options)
