77510198c7715233f553c8a6796bb9d896f413ed
mono <mohamadnobakht@gmail.com>
2021-05-03 00:35:52 +0430

    Add updater handlers package

---
+++ src/imovie/updater/handlers/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+updater handlers package.
+"""
+
+from pyrin.packaging.base import Package
+
+
+class UpdaterHandlersPackage(Package):
+    """
+    updater handlers package class.
+    """
+
+    NAME = __name__
