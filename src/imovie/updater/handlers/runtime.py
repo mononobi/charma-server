e348e9396e6460437e8636a3057d0d30ec74147b
mono <mohamadnobakht@gmail.com>
2021-05-08 01:48:49 +0430

    Implement runtime updater

---
+++ src/imovie/updater/handlers/runtime.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+updater handlers runtime module.
+"""
+
+import pyrin.utilities.string.normalizer.services as normalizer_services
+
+from imovie.updater.decorators import updater
+from imovie.updater.handlers.base import UpdaterBase
+
+
+@updater()
+class IMDBRuntimeUpdater(UpdaterBase):
+    """
+    imdb runtime updater class.
+    """
+
+    _name = 'imdb_runtime'
+    _category = 'runtime'
+
+    def _fetch(self, url, content, **options):
+        """
+        fetches data from given url.
+
+        :param str url: url to fetch info from it.
+        :param bs4.BeautifulSoup content: the html content of input url.
+
+        :returns: imdb runtime.
+        """
+
+        runtime = None
+        runtime_tag = content.find('time', datetime=True)
+        if runtime_tag is not None:
+            result = runtime_tag.get('datetime')
+            result = normalizer_services.filter(result, filters=['PT', 'M'])
+            if result.isdigit():
+                runtime = int(result)
+
+        return runtime
