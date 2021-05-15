fa828ea24db607bc3e2cccbd734964ca0c7b370a
mono <mohamadnobakht@gmail.com>
2021-05-15 22:29:18 +0430

    Implement language processor

--- src/imovie/updater/processors/language.py
+++ src/imovie/updater/processors/language.py
@@ -3,6 +3,9 @@
 updater processors language module.
 """
 
+import imovie.languages.services as language_services
+import imovie.movies.related_languages.services as related_language_services
+
 from imovie.updater.decorators import processor
 from imovie.updater.enumerations import UpdaterCategoryEnum
 from imovie.updater.processors.base import ProcessorBase
@@ -23,4 +26,19 @@ class LanguageProcessor(ProcessorBase):
         :param uuid.UUID movie_id: movie id to process data for it.
         :param list[str] data: list of languages to be processed.
         """
-        pass
+
+        related_language_services.delete_by_movie(movie_id, **options)
+        languages = []
+        for item in data:
+            language = language_services.get_by_name(item)
+            language_id = None
+            if language is not None:
+                language_id = language.id
+            else:
+                language_id = language_services.create(item, **options)
+
+            languages.append(language_id)
+
+        for index, item in enumerate(languages):
+            is_main = index == 0
+            related_language_services.create(movie_id, item, is_main=is_main)
