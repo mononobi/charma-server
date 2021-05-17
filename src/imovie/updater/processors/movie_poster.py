a3608b43a5e3746a5ea7f4f2aee295bf4ffa2dda
mono <mohamadnobakht@gmail.com>
2021-05-18 01:06:42 +0430

    Implement movie poster processor

--- src/imovie/updater/processors/movie_poster.py
+++ src/imovie/updater/processors/movie_poster.py
@@ -3,6 +3,10 @@
 updater processors movie poster module.
 """
 
+import imovie.movies.services as movie_services
+import imovie.movies.images.services as movie_image_services
+import imovie.downloader.services as downloader_services
+
 from imovie.updater.decorators import processor
 from imovie.updater.enumerations import UpdaterCategoryEnum
 from imovie.updater.processors.base import ProcessorBase
@@ -23,4 +27,8 @@ class MoviePosterProcessor(ProcessorBase):
         :param uuid.UUID movie_id: movie id to process data for it.
         :param str data: poster url to be processed.
         """
-        pass
+
+        title = movie_services.get_full_name_for_path(movie_id)
+        image_root = movie_image_services.get_root_directory()
+        _, name = downloader_services.download(data, image_root, name=title)
+        return dict(poster_name=name)
