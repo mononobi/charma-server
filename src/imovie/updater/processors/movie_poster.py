8b4ebe1d2bb120be6ee9850b79af2dee9a791b45
mono <mohamadnobakht@gmail.com>
2021-05-18 18:09:54 +0430

    Revise code

--- src/imovie/updater/processors/movie_poster.py
+++ src/imovie/updater/processors/movie_poster.py
@@ -3,6 +3,8 @@
 updater processors movie poster module.
 """
 
+from pyrin.logging.contexts import suppress
+
 import imovie.movies.services as movie_services
 import imovie.movies.images.services as movie_image_services
 import imovie.downloader.services as downloader_services
@@ -24,11 +26,20 @@ class MoviePosterProcessor(ProcessorBase):
         """
         processes given update data.
 
+        it returns None if poster downloading fails.
+
         :param uuid.UUID movie_id: movie id to process data for it.
         :param str data: poster url to be processed.
+
+        :returns: dict(str poster_name: poster name)
+        :rtype: dict
         """
 
         title = movie_services.get_full_name_for_path(movie_id)
         image_root = movie_image_services.get_root_directory()
-        _, name = downloader_services.download(data, image_root, name=title)
-        return dict(poster_name=name)
+
+        with suppress():
+            _, name = downloader_services.download(data, image_root, name=title)
+            return dict(poster_name=name)
+
+        return None
