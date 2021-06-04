84ab1e93154356ca00f5b326320cd114acefdbf1
mono <mohamadnobakht@gmail.com>
2021-06-05 01:51:16 +0430

    Implement genre v2 updater

--- src/imovie/updater/handlers/genre.py
+++ src/imovie/updater/handlers/genre.py
@@ -45,3 +45,38 @@ class GenreUpdater(UpdaterBase):
                     break
 
         return genres or None
+
+
+@updater()
+class GenreUpdaterV2(UpdaterBase):
+    """
+    genre updater v2 class.
+    """
+
+    _category = UpdaterCategoryEnum.GENRE
+
+    def _fetch(self, content, **options):
+        """
+        fetches data from given content.
+
+        :param bs4.BeautifulSoup content: the html content of imdb page.
+
+        :keyword bs4.BeautifulSoup credits: the html content of credits page.
+                                            this is only needed by person updaters.
+
+        :returns: list of imdb genres.
+        :rtype: list[str]
+        """
+
+        genres = []
+        genre_section = content.find('li', {'data-testid': 'storyline-genres'})
+        if genre_section is not None:
+            genre_list = genre_section.find('ul', class_=True)
+            if genre_list is not None:
+                genre_tags = genre_list.find_all('a', class_=True, href=True)
+                for item in genre_tags:
+                    name = item.get_text(strip=True)
+                    if len(name) > 0:
+                        genres.append(name)
+
+        return genres or None
