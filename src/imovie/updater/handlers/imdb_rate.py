935785208ee82152397a98122e00c39230fd3f8d
mono <mohamadnobakht@gmail.com>
2021-06-05 02:11:33 +0430

    Implement imdb rate v2 updater

--- src/imovie/updater/handlers/imdb_rate.py
+++ src/imovie/updater/handlers/imdb_rate.py
@@ -37,3 +37,38 @@ class IMDBRateUpdater(UpdaterBase):
                 rate = float(result)
 
         return rate
+
+
+@updater()
+class IMDBRateUpdaterV2(UpdaterBase):
+    """
+    imdb rate updater v2 class.
+    """
+
+    _category = UpdaterCategoryEnum.IMDB_RATE
+
+    def _fetch(self, content, **options):
+        """
+        fetches data from given content.
+
+        :param bs4.BeautifulSoup content: the html content of imdb page.
+
+        :keyword bs4.BeautifulSoup credits: the html content of credits page.
+                                            this is only needed by person updaters.
+
+        :returns: imdb rate.
+        :rtype: float
+        """
+
+        rate = None
+        rate_container = content.find(
+            'div', {'data-testid': 'hero-title-block__aggregate-rating__score'})
+
+        if rate_container is not None:
+            rate_tag = rate_container.find('span', class_=True)
+            if rate_tag is not None:
+                result = rate_tag.get_text(strip=True)
+                if len(result) > 0:
+                    rate = float(result)
+
+        return rate
